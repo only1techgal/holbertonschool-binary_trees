c66b56a02fd8de9306c7e3d90eb5c17bc85ef15f
only1techgal <jenn.lipholo@gmail.com>
2024-08-07 15:32:51 +0200

    18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -3,7 +3,7 @@
 #include "binary_trees.h"
 
 /**
- **binary_tree_uncle - Finds the uncle of a node
+ **binary_tree_uncle - Finds the uncle of a node.
  *@node: Pointer to the node to find the uncle
  *
  *Return: Pointer to the uncle node, NULL
