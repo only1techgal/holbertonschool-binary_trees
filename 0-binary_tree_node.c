8857ec8f3953e0257f2af3ea5c350bade6b5bd92
only1techgal <jenn.lipholo@gmail.com>
2024-08-08 11:20:46 +0200

    Remove holbertonschool-sorting_algorithms subdirectory

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_node - creates a binary tree node
-* @parent: is a pointer to the parent node of the node to create
+* binary_tree_node - creates a binary tree node.
+* @parent: is a pointer to the parent node of the node to create.
 * @value: is the value to put in the new node
 *
 * Return: a pointer to the new node, or NULL on failure
